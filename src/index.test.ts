f5232e53ce5306f25e29718ecb59f4d9b0b4cdb6
nestorochoa <nestor.ochoa@gmail.com>
2024-11-06 12:14:28 +1100

    boiler

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,12 @@
+import exp from "constants";
 import { sum } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("canary config", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it("adds 2 + 2 should not be equal 3", () => {
+    expect(sum(2, 2)).not.toBe(3);
+  });
 });
